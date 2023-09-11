69f956aead89908d462f100adcfb1a47b58ff588
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-09-11 09:13:19 +0700

    rebuild and create app

--- HorizonAPI/HorizonAPI/urls.py
+++ HorizonAPI/HorizonAPI/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('SessionManager.urls')),
     path('admin/', admin.site.urls),
 ]
--- HorizonAPI/SessionManager/views.py
+++ HorizonAPI/SessionManager/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def hompage(request):
-    return HttpResponse("discipline")
+# Create your views here.
