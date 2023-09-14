f6861017105420f5378961000767aa9bb5641216
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-09-14 11:21:42 +0700

    retrieve x-subject-token

--- HorizonAPI/SessionManager/views.py
+++ HorizonAPI/SessionManager/views.py
@@ -1,5 +1,7 @@
+from pickle import FALSE
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import requests
 from .models import User
 
 
@@ -29,16 +31,80 @@ def signup(request):
         return render(request, 'SessionManager/signup.html')
 
 
+def checkLoginKeystone(uname,pwd):
+    url = "http://127.0.0.1:5000/v3/auth/tokens"
+    payload_getID = {
+        "auth": {
+            "identity": {
+                "methods": ["password"],
+                "password": {
+                    "user": {
+                        "name": uname,
+                        "domain": {
+                            "name": "Default"
+                        },
+                        "password": pwd
+                    }
+                }
+            }
+        }
+    }
+    
+    response_getID = requests.post(url, json=payload_getID)
+    if response_getID.status_code == 201:
+        # Successful response (HTTP status code 201)
+        print("Authentication successful!")
+        # You can access the authentication token in the response headers or body, depending on the API's structure.
+        # For example, if the token is in the response body as JSON, you can access it like this:
+        user_id = response_getID.json().get("token", {}).get("user", {}).get("id")
+        print(f"user id: {user_id}")
+        payload_getToken = {    
+        "auth": {
+            "identity": {
+                "methods": [
+                    "password"
+                ],
+                "password": {
+                    "user": {
+                        "id": user_id,
+                        "password": pwd
+                    }
+                }
+            },
+                "scope": {
+                "project": {
+                    "id": "ad4728f8d4f64678b37319afba7fb85c"
+                }
+            }
+        }
+    }
+        response_getToken= requests.post(url, json=payload_getToken)
+        if response_getToken.status_code == 201:
+            print("Token successfully retreived!")
+            headers_getToken = response_getToken.headers
+            X_subject_token= headers_getToken.get('X-Subject-Token')
+            print(f"X-subject-token: {X_subject_token}")
+            return True
+        else:
+            # Handle authentication failure or other errors
+            print(f"Authentication failed. Status code: {response_getToken.status_code}")
+            print(f"Response content: {response_getToken.text}")
+            return False
+    else:
+        # Handle authentication failure or other errors
+        print(f"Authentication failed. Status code: {response_getID.status_code}")
+        print(f"Response content: {response_getID.text}")
+        return False
 
 def login(request):
     if request.method == 'POST':
         uname = request.POST.get('uname')
         pwd = request.POST.get('pwd')
-
+        
         check_user = User.objects.filter(username=uname, password=pwd)
-        if check_user:
+        if check_user and checkLoginKeystone(uname,pwd):
             request.session['user'] = uname
-            return redirect('/SessionManager/home')
+            #return redirect('/SessionManager/home')
         else:
             return HttpResponse('Please enter valid Username or Password.')
 
